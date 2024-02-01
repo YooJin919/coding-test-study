acf03155a52f54d2a9254559bb0e5e6aa04a0bb3
Yoo Jin <dbwls30131@ajou.ac.kr>
2024-02-01 22:04:36 +0900

    <SOLVE> Baekjoon 10828 스택 - silver4

---
+++ Data-Structure/Stack/baekjoon-10828.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline().rstrip())
+stack = deque()
+
+for _ in range(T):
+  order = deque(sys.stdin.readline().split())
+  if(order[0] == "push"):
+    stack.append(int(order[1]))
+  elif(order[0] == "pop"):
+    if(len(stack) == 0):
+      print("-1")
+    else:
+      print(stack.pop())
+  elif(order[0] == "size"):
+    print(len(stack))
+  elif(order[0] == "empty"):
+    if(len(stack) == 0):
+      print("1")
+    else:
+      print("0")
+  elif(order[0] == "top"):
+    if(len(stack) == 0):
+      print("-1")
+    else:
+      print(stack[-1])
