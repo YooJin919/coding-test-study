42e567d29f1e62f651483f0d277f03b0f9d36b61
YooJin919 <jiny08360@gmail.com>
2025-04-24 21:13:04 +0900

    <SOLVE> Baekjoon 정수 삼각형 - 실버1

---
+++ Dynamic-Programming/baekjoon-1932.py
@@ -0,0 +1,19 @@
+# 삼각형 모양
+# 크기 N = 한 변의 길이 N
+# 꼭대기 -> 1층까지 갈 수 있는 경로 중 선택된 수의 최대 합 구하기
+
+from sys import stdin
+
+N = int(stdin.readline().rstrip())
+triangle = []
+sum = [[0] * N for _ in range(N)]
+
+for _ in range(N):
+  triangle.append(list(map(int, stdin.readline().split())))
+
+sum[0][0] = triangle[0][0]
+for i in range(1 ,N):
+  for j in range(i+1):
+    sum[i][j] = max(sum[i-1][j]+triangle[i][j], sum[i-1][j-1]+triangle[i][j])
+    
+print(max(sum[N-1]))
